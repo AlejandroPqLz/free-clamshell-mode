8c6a5d08a7d9aa009054626f054bb692bc22170d
AlejandroPqLz <pq.lz.alejandro@gmail.com>
2026-05-08 03:23:35 +0200

    Update build script to use PNG format for app icon generation

--- build.sh
+++ build.sh
@@ -15,9 +15,9 @@ mkdir -p "$APP_BUNDLE/Contents/Resources"
 ICONSET="$BUILD_DIR/AppIcon.iconset"
 mkdir -p "$ICONSET"
 for size in 16 32 64 128 256 512; do
-    sips -s format png -z $size $size free-nautilus-shell-logo.jpg --out "$ICONSET/icon_${size}x${size}.png" > /dev/null
+    sips -s format png -z $size $size free-nautilus-shell-logo.png --out "$ICONSET/icon_${size}x${size}.png" > /dev/null
     double=$((size * 2))
-    sips -s format png -z $double $double free-nautilus-shell-logo.jpg --out "$ICONSET/icon_${size}x${size}@2x.png" > /dev/null
+    sips -s format png -z $double $double free-nautilus-shell-logo.png --out "$ICONSET/icon_${size}x${size}@2x.png" > /dev/null
 done
 iconutil -c icns "$ICONSET" -o "$APP_BUNDLE/Contents/Resources/AppIcon.icns"
 rm -rf "$ICONSET"
